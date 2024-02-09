a7d8f5b34cf7b455273ce40887c193d252f34782
Hebx <lord.noctx@gmail.com>
2024-02-09 18:04:15 +0100

    Added Deploy Script of the engineand Helper config with pricefeeds and mocks

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,27 @@
+// SPDX-LICENSE-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployDsc is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+        HelperConfig config = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            config.activeNetworkConfig();
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        tokenAddresses = [weth, wbtc];
+        vm.startBroadcast(deployerKey);
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engline = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(engline));
+        vm.stopBroadcast();
+        return (dsc, engline);
+    }
+}
